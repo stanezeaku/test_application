27c7a0aa63c0db258522325ff560faa126967b33
Ezeaku Ifechukwu Stanley <56836874+stanezeaku@users.noreply.github.com>
2023-07-12 15:56:18 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Test Application'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  List colors = Colors.primaries;
+  int colorIndex = Random().nextInt(Colors.primaries.length);
+
+  // This method randomly changes color from the colors list.
+  void changeColor() {
+    setState(() => colorIndex = Random().nextInt(Colors.primaries.length));
+  }
+
+  late AnimationController controller;
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    rotation = Tween<double>(
+      begin: 0,
+      end: 2 * pi,
+    ).animate(controller);
+
+    controller.animateTo(1);
+  }
+
+  @override
+  void dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: colors[colorIndex],
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(
+          widget.title,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: changeColor,
+        child: Container(
+          color: colors[colorIndex],
+          height: double.infinity,
+          width: double.infinity,
+          child: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: AnimatedBuilder(
+                animation: controller,
+                builder: (_, child) =>
+                    Stack(alignment: Alignment.center, children: <Widget>[
+                      Transform(
+                        transform: Matrix4.rotationZ(rotation.value),
+                        alignment: Alignment.center,
+                        child: const Text('Hello there',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ])),
+          ),
+        ),
+      ),
+    );
+  }
+}
